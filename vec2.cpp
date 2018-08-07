bc6e3ab1f0731c3c547267e1f046640cc1549c34
Daniel <danieltse7@gmail.com>
2018-08-07 15:00:31 +0000

    operator overload.

--- vec2.cpp
+++ vec2.cpp
@@ -1,3 +1,5 @@
+#define _SCL_SECURE_NO_WARNINGS
+
 #include "std_lib_facilities.h"
 
 //using namespace std;
@@ -15,50 +17,27 @@ public:
 
 	~vect() { delete[] elem; }
 
-	double get(int i) { return elem[i]; }
-	void set(int i, double d) { elem[i] = d; }
+	double& operator[](int n) { return elem[n]; }
 
-	/*vect& operator=(const vect& a) {
-		double* p = new double[a.sz];
-		copy(a.elem, a.elem + a.sz, p);
-		delete[] elem;
-		elem = p;
-		sz = a.sz;
-		return *this;
-	}*/
+	//double get(int i) { return elem[i]; }
+	//void set(int i, double d) { elem[i] = d; }
 private:
 	int sz;
 	double* elem;
 };
 
-//vect::vect(const vect& arg)
-//// allocate space, then initialize via copy
-//	:sz{ arg.sz }, elem{ new double[arg.sz] }
-//{
-//	copy(arg.elem, arg.elem + arg.sz, elem);  // from std lib
-//}
-//
-//vect& vect::operator=(const vect& a) {
-//	double* p = new double[a.sz];
-//	copy(a.elem, a.elem + a.sz, p);
-//	delete[] elem;
-//	elem = p;
-//	sz = a.sz;
-//	return *this;
-//}
-
 void f(int n) {
 	vect v(3);
-	v.set(2, 2.2);
+	v[2] = 2.2;//v.set(2, 2.2);
 	vect v2 = v;
-	v.set(1, 9.9);
-	v2.set(0, 8.8);
-	cout << v.get(0) << ' ' << v2.get(1);
+	v[2] = 9.9;//v.set(1, 9.9);
+	v2[0] = 8.8;//v2.set(0, 8.8);
+	cout << v[0] << ' ' << v2[1];
+	//cout << v.get(0) << ' ' << v2.get(1);
 }
 
 int main() {
-	//while (true) f(10);
-	int i;
+	int i = 0;
 	while (i < 10000) {
 		f(10);
 		i++;
